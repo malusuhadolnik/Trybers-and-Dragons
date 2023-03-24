70ec0fdc324d90ad8c77964e2a624b0bd57ad463
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-03-24 19:43:32 -0300

    -feat: criação dos métodos da interface Fighter

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,8 +1,12 @@
-import Energy from "../Energy"
+import Energy from '../Energy';
 
 export default interface Fighter {
   lifePoints: number,
   strength: number,
   defense: number,
-  energy: Energy,
+  energy?: Energy,
+  attack(enemy: Fighter): void,
+  special?(enemy: Fighter): void,
+  levelUp(): void,
+  receiveDamage(attackPoints: number): number,
 }
