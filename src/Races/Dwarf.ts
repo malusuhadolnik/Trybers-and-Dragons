73be9cc689aa61a54473bac5185d08719571f5b8
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-03-24 17:03:25 -0300

    feat - criação da classe Dwarf

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,16 @@
+import Race from './Race';
+
+export default class Dwarf extends Race {
+  public maxLifePoints: number;
+  static dwarfCount = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this.maxLifePoints = 80;
+    Dwarf.dwarfCount += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return this.dwarfCount
+  }
+}
